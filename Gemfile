74a26abfdbda2ae62c78687c2f41a38bd8cf474d
Hamid Siddiqui <inventionsbyhamid@gmail.com>
2017-02-16 00:29:39 +0530

    add navbar

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'materialize-sass'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'bcrypt', '~> 3.1.7', platforms: [:ruby]
+gem 'bcrypt', '~> 3.1.11', platforms: [:ruby]
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :email, :password, :password_confirmation, :contact])
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "devise"
+    else
+      "application"
+    end
   end
 
   def index
