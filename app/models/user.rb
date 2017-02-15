7f08629d8cdb2f97db1bfebe73a880cf6bc23355
Hamid Siddiqui <inventionsbyhamid@gmail.com>
2017-02-15 21:31:06 +0530

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates :contact, presence: true, length: {maximum: 13, minimum: 10}
+  validates :full_name, presence: true, length: {maximum: 25, minimum: 3}
+  
 end
