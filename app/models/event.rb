96da7c26b8847c8be67e35eb84646f74c1a2a8f3
Hamid Siddiqui <inventionsbyhamid@gmail.com>
2017-03-13 23:53:56 +0530

    layout changes, rule blank

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,6 @@ class Event < ApplicationRecord
 
 	validates :title, presence: true, length: {maximum: 40}
   	validates :description, presence: true, length: {minimum: 20}
-  	validates :rules, presence: true, length: {minimum: 10}
   	validates :location, presence: true, length: {maximum: 30, minimum: 3}
   	validates :date_time, presence: true, length: {maximum: 20, minimum: 3}
   	validates :day, presence: true, :numericality => { :greater_than => 0 }
